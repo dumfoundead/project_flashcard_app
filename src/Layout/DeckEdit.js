1a414759d19720e7c9dc67e9cb7bfccbfef2dda9
Tien Vu <69743095+dumfoundead@users.noreply.github.com>
2021-04-14 22:51:33 -0400

    Update DeckEdit.js

--- src/Layout/DeckEdit.js
+++ src/Layout/DeckEdit.js
@@ -38,7 +38,6 @@ function DeckEdit(){
     };
     const history = useHistory();
     const handleSubmit = (event) => {
-        let output = [];
         event.preventDefault();
         //console.log("Submitted:", deck);
         async function updateData() {
